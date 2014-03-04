1e3ddb482b7ff0beb2fe9abdb8262495445bdf73
scotartt <scot.mcphee@gmail.com>
2014-03-04 18:29:50 +1000

    added ability to edit group and add users to group. rather than groups to users. will be easier to who's not got the permission and bulk edit.

--- decommentariis/decommentariis/admin.py
+++ decommentariis/decommentariis/admin.py
@@ -1,6 +1,13 @@
+from django import forms
 from django.contrib import admin
+from django.contrib import admin
+from django.contrib.admin.widgets import FilteredSelectMultiple
+from django.contrib.auth.models import User, Group
 from django.contrib.sites.models import Site
+from django.utils.translation import ugettext_lazy as _
+
 from decommentariis.models import TEIEntry, TEISection, CommentaryEntry
+
 
 class TEIEntryAdmin(admin.ModelAdmin):
 	pass
@@ -11,7 +18,45 @@ class TEISectionAdmin(admin.ModelAdmin):
 
 class CommentaryEntryAdmin(admin.ModelAdmin):
 	pass
-	
+
 admin.site.register(TEIEntry, TEIEntryAdmin)
 admin.site.register(TEISection, TEISectionAdmin)
 admin.site.register(CommentaryEntry, CommentaryEntryAdmin)
+
+class GroupAdminForm(forms.ModelForm):
+	users = forms.ModelMultipleChoiceField(
+		queryset=User.objects.all(), 
+		required=False,
+		widget=FilteredSelectMultiple(
+			verbose_name=_('Users'),
+			is_stacked=False
+		)
+	)
+
+	class Meta:
+		model = Group
+
+	def __init__(self, *args, **kwargs):
+		super(GroupAdminForm, self).__init__(*args, **kwargs)
+
+		if self.instance and self.instance.pk:
+			self.fields['users'].initial = self.instance.user_set.all()
+
+	def save(self, commit=True):
+		group = super(GroupAdminForm, self).save(commit=False)
+
+		if commit:
+			group.save()
+
+		if group.pk:
+			group.user_set = self.cleaned_data['users']
+			self.save_m2m()
+
+		return group
+
+class GroupAdmin(admin.ModelAdmin):
+	form = GroupAdminForm
+	
+
+admin.site.unregister(Group)
+admin.site.register(Group, GroupAdmin)
